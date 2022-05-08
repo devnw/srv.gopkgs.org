a956b2f75000614fe0253902a095470a00dd5d34
Benji Vesterby <benji@devnw.com>
2022-05-08 16:51:36 -0400

    Minor updates

--- api/cors.go
+++ api/cors.go
@@ -9,21 +9,19 @@ import (
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Origin
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		headers := rw.Header()
-
 		// Allow-Origin header shall be part of ALL the responses
-		headers.Add("Access-Control-Allow-Origin", "*")
+		rw.Header().Add("Access-Control-Allow-Origin", "*")
 		if req.Method != http.MethodOptions {
 			next.ServeHTTP(rw, req)
 			return
 		}
 
 		// process an HTTP OPTIONS preflight request
-		headers.Add("Access-Control-Allow-Headers", "Authorization")
+		rw.Header().Add(
+			"Access-Control-Allow-Headers",
+			"Authorization,Content-Type",
+		)
 		rw.WriteHeader(http.StatusNoContent)
-		_, err := rw.Write(nil)
-		if err != nil {
-			alog.Println(err, "http response (options) write error")
-		}
+		alog.Println("CORS preflight request")
 	})
 }
